324ce1dff04d6ac8070bc57469edcd1617d6d093
Nicolas De Loof <nicolas.deloof@gmail.com>
2025-02-21 10:08:52 +0100

    introduce pull policy to refresh image every xx
    Signed-off-by: Nicolas De Loof <nicolas.deloof@gmail.com>

--- loader/loader_test.go
+++ loader/loader_test.go
@@ -3744,3 +3744,17 @@ services:
 	assert.Equal(t, len(p.Services["test"].Gpus), 1)
 	assert.Equal(t, p.Services["test"].Gpus[0].Count, types.DeviceCount(-1))
 }
+
+func TestPullRefresh(t *testing.T) {
+	p, err := loadYAML(`
+name: load-all-gpus
+services:
+  test:
+    pull_policy: every_2d
+`)
+	assert.NilError(t, err)
+	policy, duration, err := p.Services["test"].GetPullPolicy()
+	assert.NilError(t, err)
+	assert.Equal(t, policy, types.PullPolicyRefresh)
+	assert.Equal(t, duration, 2*24*time.Hour)
+}
